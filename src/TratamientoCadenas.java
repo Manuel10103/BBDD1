8823a7f9ce1cdcd30bcccf4a90766463e4daebee
Manuel10103 <mgolderos66@gmail.com>
2023-05-25 09:30:38 +0200

    commit tratamientocadenas

--- src/TratamientoCadenas.java
+++ src/TratamientoCadenas.java
@@ -1,9 +1,10 @@
+package src;
 
 public class TratamientoCadenas {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Esta es la clase de TratamientoCadenas");
+		// TODO Auto-generated method stub
+
 	}
 
 }
